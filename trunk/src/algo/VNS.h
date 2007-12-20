ea8bba661e666a5cd5a1d5e07db762edc22ce5a8
fortinmat <fortinmat@81f11f40-563e-0410-b35f-45f700be9b36>
2007-12-20 22:48:34 +0000

    Impossible solution bug (that loops forever) fixed =)

--- trunk/src/algo/VNS.h
+++ trunk/src/algo/VNS.h
@@ -10,11 +10,8 @@ private:
 public:
 	VNS();
 	VNS(int _iterationLimit);
-
 	~VNS();
 
-	Solution getOptimumLocal(DetQuadProblem pb, Solver &s, const Solution& sol) const;
-	
 	virtual Solution solve(DetQuadProblem& pb, Solver &s) const;
 };
 
