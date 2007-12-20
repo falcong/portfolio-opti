4815abc3e49d5cd6dfa7dbc6e2b5eb9d5c9bcc43
antoinelangevin <antoinelangevin@81f11f40-563e-0410-b35f-45f700be9b36>
2007-12-20 16:36:26 +0000

    Update VNS part Added :VNS.h,VNS.cpp VNS part create but not tested. TODO: test (Windows Issue)

---
+++ trunk/src/algo/VNS.cpp
@@ -0,0 +1,75 @@
+#include "VNS.h"
+
+VNS::VNS()
+{
+	iterationLimit = 0;
+}
+
+VNS::VNS( int _iterationLimit)
+{
+	iterationLimit = _iterationLimit;
+}
+
+VNS::~VNS()
+{
+	
+}
+
+Solution VNS::solve(DetQuadProblem pb, Solver * s)
+{
+	if (iterationLimit == 0)
+		iterationLimit = pb.getK()/2;
+
+	LinearProblem lp = pb.getSimpleLinearProblem();
+	
+	int i = 2, x;
+	Solution sol = s->getAdmissibleSolution(&lp);
+	Solution xPrim, xSecond;
+
+	
+	int max = pb.objectiveFunction(sol);
+
+	while( i < iterationLimit)
+	{
+		xPrim = lp.getNeighbour(sol, i);
+		lp = pb.getFixedLP(xPrim);
+		xPrim = s->getAdmissibleSolution(&lp);
+		xSecond = getOptimumLocal(pb, s, xPrim);
+
+		if(pb.objectiveFunction(xSecond)>max)
+		{
+			sol = xSecond;
+			max = pb.objectiveFunction(xSecond);
+			i=2;
+		}
+		else i++;
+	}
+	return sol;
+}
+
+Solution VNS::getOptimumLocal(DetQuadProblem pb, Solver * s, const Solution& sol) const
+{
+	Solution res = sol;
+	LinearProblem lp = pb.getFixedLP(res);
+	res = s->getAdmissibleSolution(&lp);
+
+	int maxTemp = pb.objectiveFunction(res);
+	
+	Solution vTemp;
+
+	int cpt = lp.getK() * (lp.getN() - lp.getK());
+
+	for(int j = 0 ; j< cpt ; j++)
+	{
+		vTemp = lp.getNeighbour(sol, 1);
+		lp = pb.getFixedLP(vTemp);
+		vTemp = s->getAdmissibleSolution(&lp);
+		if(pb.objectiveFunction(vTemp) > maxTemp)
+		{
+			maxTemp = pb.objectiveFunction(vTemp);
+			res = vTemp;
+		}
+	}
+	return res;
+
+}
