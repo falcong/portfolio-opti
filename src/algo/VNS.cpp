e53da37498be3fe302b7dc3a32694eb33ade2a19
fortinmat <fortinmat@81f11f40-563e-0410-b35f-45f700be9b36>
2007-12-22 14:23:10 +0000

    Infinite loop on empty solution fixed for the second time ...

--- src/algo/VNS.cpp
+++ src/algo/VNS.cpp
@@ -22,7 +22,7 @@ Solution VNS::solve(DetQuadProblem& pb, Solver& s) const {
 
 	int i = 2;
 	Solution sol = s.getAdmissibleSolution(&lp);
-	if(sol.isNull()) {
+	if (sol.isNull()) {
 		return sol;
 	}
 	Solution xPrim, xSecond;
@@ -31,26 +31,27 @@ Solution VNS::solve(DetQuadProblem& pb, Solver& s) const {
 #ifdef DEBUG
 	std::cout << "Risk = " << min << " : "<< sol.toString() << std::endl;
 #endif
-	
+
 	while (i < itLim) {
 		xPrim = lp.getNeighbour(sol, i);
 		lp = pb.getFixedLP(xPrim);
+		lp.print();
 		xPrim = s.getBestSolution(&lp);
-		if(xPrim.isNull()) {
+		if (xPrim.isNull()) {
 			++i;
-			continue;
-		}
-
-		float risk = pb.objectiveFunction(xPrim);
-		if (risk < min) {
-			sol = xPrim;
-			min = risk;
-			i=2;
-#ifdef DEBUG
-			std::cout << "Risk = " << min << " : "<< sol.toString() << std::endl;
-#endif
 		} else {
-			++i;
+
+			float risk = pb.objectiveFunction(xPrim);
+			if (risk < min) {
+				sol = xPrim;
+				min = risk;
+				i=2;
+#ifdef DEBUG
+				std::cout << "Risk = " << min << " : "<< sol.toString() << std::endl;
+#endif
+			} else {
+				++i;
+			}
 		}
 	}
 	sol.setZ(min);
